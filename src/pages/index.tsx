33ab4288aa805fd22568e228589427e538ccae3f
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-03 16:05:52 -0300

    :pencil2: Configuring extern font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>Óh Senhor Jesus!</h1>
+    <h1 className={styles.title}>
+      Óh Senhor Jesus!
+    </h1>
   )
 }
