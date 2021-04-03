f73537359e780c1fadcded2538104710f76c13e8
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-03 16:12:48 -0300

    Dynamic Title per page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import Head from 'next/head'
+
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Óh Senhor Jesus!
-    </h1>
+    <>
+      {/* Todo conteúdo que eu coloco dento do componente Head(do Next) vai para o cabeçalho do html(head), podemos posicionar ele em qualquer lugar que ele sempre vai levar o conteúdo para head */}
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Óh Senhor Jesus!
+      </h1>
+    </>
   )
 }
