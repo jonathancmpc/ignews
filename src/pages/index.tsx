8abb0b050b010357b8672ee4423d3bfc97d486ae
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-03 16:23:46 -0300

    Global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Óh Senhor Jesus!
       </h1>
     </>
