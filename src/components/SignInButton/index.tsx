78c4511c80ea90909ec9c93437e9864bdcbafc88
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-05 17:36:55 -0300

    GitHub authentication with NextAuth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession(); //Verifica se o usuário está logado no github
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361"/>
-      Jonathan Cavalcante
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')} // Essa é uma função do NextAuth, devemos passar o nome do aplicativo social que o usuário vai logar
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { Provider as NextAuthProvider } from 'next-auth/client'; //Provider para passar a informação do usuário logado para todas as páginas
 
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}> {/* As informações da sessão são gravadas no pageProps */}
       <Header />
       <Component {...pageProps} />
-    </>
+    </NextAuthProvider>
   );
 }
 
