16354c915aa0f918e75f2a32613ad7c878813897
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-06 10:39:42 -0300

    Inserting the user's email in the DB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   //Configure one or more authentication providers
@@ -11,4 +15,24 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user; //pegando e-mail do usuário que retornou como calback do github ao fazer a autenticação
+
+      try {
+        //inserindo no banco utilizando a linguagem de consulta do Fauna chamado FQL
+        await fauna.query(
+          q.Create(
+            q.Collection('users'), //nome da tabela
+            { data: { email } } //dados do usuário que queremos inserir
+          )
+        )
+  
+        return true
+
+      } catch {
+        return false        
+      }
+    },
+  },
 });
