70f09591c7904f20ab35c87dbde638016e7ef4e3
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-07 19:19:22 -0300

    If exists user Upload FaunaDB

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,58 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import {query as q} from 'faunadb';
 import { getSession } from 'next-auth/client';
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id:string;
+  },
+  data: {
+    stripe_customer_id: string;
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     //criamos primeiramente o perfil do cliente dentro do stripe, pegamos as informações através do cookies no método getSession do Next-Auth
     const session = await getSession({ req });
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id;
+
+    // Se não existir um id do stripe no banco, criamos um
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+      });
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id,
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id;
+    }
     
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-    });
+
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id, //quem está comprando o produto, é passado o id do usuário do stripe
+      customer: customerId, //quem está comprando o produto, é passado o id do usuário do stripe
       payment_method_types: ['card'], // pagamento com cartão
       billing_address_collection: 'required', // requeremos o endereço do comprador
       line_items: [
