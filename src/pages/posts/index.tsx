3fd99ccef5d35c30e0949ccf4f7a1df0e81d74cb
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-09 13:46:38 -0300

    Connecting from API Prismic CMS

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,52 @@
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+import Head from 'next/head'
+import { getPrismicClient } from '../../services/prismic';
+import styles from './styles.module.scss'
+
+export default function Posts() {
+  return(
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Upload de imagens no Front End com ReactJS e Context API</strong>
+            <p>Vamos implementar o sistema de upload de imagens no Front End com ReactJS utilizando o gerenciamento de estados com Context API.</p>
+          </a>
+          <a>
+            <time>12 de março de 2021</time>
+            <strong>Upload de imagens no Front End com ReactJS e Context API</strong>
+            <p>Vamos implementar o sistema de upload de imagens no Front End com ReactJS utilizando o gerenciamento de estados com Context API.</p>
+          </a>
+          <a>
+            <time>12 de março de 2021</time>
+            <strong>Upload de imagens no Front End com ReactJS e Context API</strong>
+            <p>Vamos implementar o sistema de upload de imagens no Front End com ReactJS utilizando o gerenciamento de estados com Context API.</p>
+          </a>
+        </div>        
+      </main>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'post')
+  ], {
+    fetch: ['post.title', 'post.content'],
+    pageSize: 100,
+  })
+
+  console.log(response);
+
+  return {
+    props: {}
+  }
+}
