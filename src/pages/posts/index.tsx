2f4ced4080db6303f24d2e3ec78c7826e9d606cd
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-09 17:23:05 -0300

    Listing screen posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,22 @@
 import { GetStaticProps } from 'next';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
 import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic';
 import styles from './styles.module.scss'
 
-export default function Posts() {
+interface Posts {
+  slug: string,
+  title: string,
+  excerpt: string,
+  updatedAt: string
+}
+
+interface PostsProps {
+  posts: Posts[]
+}
+
+export default function Posts({ posts }:PostsProps) {
   return(
     <>
       <Head>
@@ -13,21 +25,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Upload de imagens no Front End com ReactJS e Context API</strong>
-            <p>Vamos implementar o sistema de upload de imagens no Front End com ReactJS utilizando o gerenciamento de estados com Context API.</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Upload de imagens no Front End com ReactJS e Context API</strong>
-            <p>Vamos implementar o sistema de upload de imagens no Front End com ReactJS utilizando o gerenciamento de estados com Context API.</p>
-          </a>
-          <a>
-            <time>12 de março de 2021</time>
-            <strong>Upload de imagens no Front End com ReactJS e Context API</strong>
-            <p>Vamos implementar o sistema de upload de imagens no Front End com ReactJS utilizando o gerenciamento de estados com Context API.</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href='#'>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }
         </div>        
       </main>
     </>
@@ -44,9 +48,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(response);
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
