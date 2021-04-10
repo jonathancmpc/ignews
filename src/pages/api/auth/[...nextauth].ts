f77c84077a3cfb5b1d04964382ea798ff475dfdb
Jonathan Cavalcante <jonathancmpc@gmail.com>
2021-04-10 11:43:08 -0300

    Validate subscription active

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { query as q } from 'faunadb';
 
 import NextAuth from 'next-auth';
+import { session } from 'next-auth/client';
 import Providers from 'next-auth/providers';
 
 import { fauna } from '../../../services/fauna';
@@ -15,7 +16,49 @@ export default NextAuth({
     }),
   ],
   callbacks: {
+    // Pegando dados da sessão e verificando se tem uma assinatura ativa
+    async session(session) {
+
+      try {
+
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        }
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null
+        }
+      }
+
+    },
+    //Fazendo login
     async signIn(user, account, profile) {
+      
       const { email } = user; //pegando e-mail do usuário que retornou como calback do github ao fazer a autenticação
 
       try {
